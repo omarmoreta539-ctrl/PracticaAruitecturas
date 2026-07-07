e90d9aa59ce0765cbea85eb06c90139be33c11ce
omarmoreta539-ctrl <omar.moreta539@ist17dejulio.edu.ec>
2026-07-07 08:46:22 -0500

    Proyecto Incial con Interfaz Humanp

---
+++ src/main/java/Humano.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
+/**
+ *
+ * @author AMARU
+ */
+public interface Humano {
+    //2 Cosas
+    // Una constante Final y Valor
+    //Metodos declarados no implementados terminan en
+    
+    public void identificacion();
+    
+}
